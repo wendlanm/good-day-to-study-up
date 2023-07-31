0e6e3639c4fc98cb42ebfff233fcca47bb115b05
wendlanm <wendlanm@oregonstate.edu>
2023-07-31 11:20:03 -0600

    Initial commit

---
+++ example_microservice_client.py
@@ -0,0 +1,48 @@
+# Example microservice client implementation
+# CS361 Summer 2023 Assignment 9
+# Michael Wendland
+
+import zmq
+import time
+
+
+def convert_dict_to_bytes(input_dict):
+    return str(input_dict).encode()
+
+
+context = zmq.Context()
+
+# Initialize socket to talk to microservice
+print("Connecting to leaderboard microservice...")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:8888")
+print("Connected!")
+
+# Request the current leaderboard
+time.sleep(10)
+print("Sending request for current leaderboard")
+socket.send(b"get_leaderboard")
+
+# Get the reply
+leaderboard_data = socket.recv()
+json_data = leaderboard_data.decode('utf-8')
+print(f"Received leaderboard data: {json_data}")
+
+# Add new data to leaderboard
+new_data = {
+    "name": "New Person",
+    "score": 20,
+}
+
+time.sleep(10)
+
+socket.send(b"add_to_leaderboard")
+response = socket.recv()
+if response == b"ready":
+    socket.send(convert_dict_to_bytes(new_data))
+    response = socket.recv()
+if response == b"added":
+    print("Successfully added new score to leaderboard")
+    time.sleep(5)
+else:
+    print("Failed to add")
---
+++ leaderboard.py
@@ -0,0 +1,61 @@
+# Microservice implementation
+# CS361 Summer 2023 Assignment 9
+# Michael Wendland
+
+import zmq
+import time
+import json
+
+
+def write_leaderboard(leaderboard_dict):
+    """Write leaderboard dict to json file"""
+    with open("./leaderboard/leaderboard.json", "w") as outfile:
+        outfile.write(json.dumps(leaderboard_dict))
+
+
+def get_leaderboard(return_dict=False):
+    """Get current leaderboard and return as either dict or bytes"""
+    with open('./leaderboard/leaderboard.json', 'r') as f:
+        # Read from json file
+        # Return either the dict or the bytes depending on argument
+        if return_dict:
+            return json.load(f)
+        else:
+            leaderboard_data = str(json.load(f))
+    return leaderboard_data.encode()
+
+
+def add_score(new_score):
+    """Add new high score to leaderboard"""
+    formatted_data = new_score.decode('utf-8')
+    formatted_data = formatted_data.replace("\'", "\"")
+    formatted_data = json.loads(formatted_data)
+    print(f"type: {type(formatted_data)}")
+    leaderboard = get_leaderboard(return_dict=True)
+    leaderboard["leaderboard"].append(formatted_data)
+    write_leaderboard(leaderboard)
+
+
+# Start socket
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:8888")
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print(f"Received request: {message}")
+
+    if message == b"get_leaderboard":
+        leaderboard = get_leaderboard()
+        time.sleep(5)
+        socket.send(leaderboard)
+    elif message == b"add_to_leaderboard":
+        socket.send(b"ready")
+        new_data = socket.recv()
+        print(f"Preparing to add {new_data} to leaderboard")
+        add_score(new_data)
+        time.sleep(5)
+        socket.send(b"added")
+
+    time.sleep(5)
